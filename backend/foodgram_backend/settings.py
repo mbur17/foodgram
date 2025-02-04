b35181b67b59539c4daecb05af0854acc310a2f2
Maxim Buryakovsky <mbur1717@gmail.com>
2025-02-04 11:09:17 +0300

    add CSRF

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,6 +11,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
